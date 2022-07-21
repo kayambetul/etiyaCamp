98a33b1f877a7fb63b2fc29f279791dce370e701
[betulkayam24@gmail.com] <betulkayam24@gmail.com>
2022-07-21 17:25:22 +0300

    first commit

---
+++ methodOverloading/src/methodOverloading/DortIslem.java
@@ -0,0 +1,14 @@
+package methodOverloading;
+
+public class DortIslem {
+	public int Topla(int sayi1,int sayi2) {
+		return sayi1+sayi2;
+	}
+
+	public int Topla(int sayi1,int sayi2,int sayi3) {
+		return sayi1+sayi2;
+	}
+	
+
+	//aynı metot farklı parametre olma durumu
+}
