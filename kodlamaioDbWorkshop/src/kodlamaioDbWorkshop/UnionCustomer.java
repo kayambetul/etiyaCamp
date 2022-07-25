58f439284fc0d5d5c31aba3272ed4ccaabfdbb95
[betulkayam24@gmail.com] <betulkayam24@gmail.com>
2022-07-25 15:09:02 +0300

    first commit

---
+++ kodlamaioDbWorkshop/src/kodlamaioDbWorkshop/UnionCustomer.java
@@ -0,0 +1,28 @@
+package kodlamaioDbWorkshop;
+
+public class UnionCustomer extends Customer{
+
+	private String name;
+
+
+	public UnionCustomer() {
+		super();
+	}
+
+	
+	public UnionCustomer(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
